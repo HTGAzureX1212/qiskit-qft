f2491abf1a650f50b28104c83c6aedac8f39448c
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2023-11-04 16:05:05 +0800

    fix(qiskit_qft): what...

--- qiskit_qft/__init__.py
+++ qiskit_qft/__init__.py
@@ -31,4 +31,4 @@ def qft(circuit: QuantumCircuit, n: int) -> None:
 def inverse_qft(circuit: QuantumCircuit, n: int) -> None:
   temp_circuit = QuantumCircuit(n)
   qft(temp_circuit, n)
-  circuit += temp_circuit.inverse()
+  circuit = circuit + temp_circuit.inverse()
