dcc78fd04de9a2f2e70f027007dac2027ec10c92
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2023-11-04 15:36:10 +0800

    feature(qiskit_qft): add QFT implementation

---
+++ qiskit_qft/__init__.py
@@ -0,0 +1,31 @@
+from math import py
+from qiskit import QuantumCircuit
+
+def rotations(circuit: QuantumCircuit, n: int) -> QuantumCircuit:
+  assert n >= 0
+
+  if n == 0:
+    return circuit
+
+  q1 = n - 1
+  while q1 >= 0:
+    circuit.h(q1)
+
+    for q2 in range(q1):
+      circuit.cp(pi / 2**(q1 - q2), q2, q1)
+
+    circuit.barrier()
+
+    q1 -= 1
+
+
+def swap_all(circuit: QuantumCircuit, n: int):
+  for q in range(n // 2):
+    circuit.swap(q, n - q - 1)
+
+
+def qft(circuit: QuantumCircuit, n: int) -> QuantumCircuit:
+  rotations(circuit, n)
+  swap_all(circuit, n)
+
+  return circuit
